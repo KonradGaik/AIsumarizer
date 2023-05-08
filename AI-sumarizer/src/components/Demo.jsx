50d052117430087f03cb6a2ee7182e2ea3fadaf4
Konrad <konradgaik@icloud.com>
2023-05-08 12:14:57 +0200

    summarize fixed

--- AI-sumarizer/src/components/Demo.jsx
+++ AI-sumarizer/src/components/Demo.jsx
@@ -10,7 +10,7 @@ const Demo = () => {
     })
     const [allArticles, setAllArticles] = useState([]);
     const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery()
-
+    const [copied, setCopied] = useState('')
     useEffect(() => {
       
         const articlesFromLocalStorage = JSON.parse(
@@ -22,7 +22,11 @@ const Demo = () => {
 
     }, [])
     
-    
+    const handleCopy = ((copyUrl) => {
+        setCopied(copyUrl);
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(() => setCopied(false),2000)
+    })
     const handleSubmit = async (e) => {
         e.preventDefault()
         const { data } = await getSummary({
@@ -50,17 +54,45 @@ const Demo = () => {
             </form>
 
             <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-                {allArticles.map((item,index) => {
-                    <div key={`link-${index}`}
-                     onClick={() => setArticle(item)} className="link_card">
-                        <div className="copy_btn">
-                            <img src={copy} alt='copy-icon' className="w-[40%] h-[40%] object-contain" />
-                        </div>
-                        <p>test</p>
-                    </div>
-                })}
+            {allArticles.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className='link_card'
+            >
+              <div className='copy_btn' onClick={() => (handleCopy(item.url))}>
+                <img src={copied === item.url ? tick : copy} alt='copy_btn' />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+            </div>
+          ))}
             </div>
 
+        </div>
+
+        <div className="my-10 max-w-full flex justify-center items-center">
+            { isFetching ? (
+                <img className="w-20 h-20 object-contain" src={loader} alt='loader'/>
+            ) : error ? (
+                <p>
+                    Well, that wasn't suppose to happen
+                </p>
+            ) : (
+                article.summary && (
+                    <div className="flex flex-col gap3">
+                        <h2 className="text-2xl text-blue-500 my-3">
+                            Article Summary
+                        </h2>
+                        <div>
+                            <p>{article.summary}</p>
+                        </div>
+                        </div>
+                )
+            )
+            
+            }
         </div>
     </section>
   )
